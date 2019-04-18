e73a1996e75cfceb523bf223bbad6228e6e19c81
Tomasz Sachanowski <tomaszsachanowski@gmail.com>
2019-04-18 09:45:12 +0200

    write docstring and change class names

--- widget.py
+++ widget.py
@@ -6,61 +6,118 @@ from matplotlib.widgets import Button, SpanSelector, Slider, RadioButtons
 import crystlib
 
 
-class Bttn_bins(Button):
-    """
-    Klasa reprezentująca guzik do zmiany ilości
-    bins we wszystkich histogrmaach
+class ButtonBins(Button):
+    """A GUI button to change the number of bins in all histograms
+
+    Attributes
+    ----------
+    fig : The :class:`matplotlib.figure.Figure`.
+
+        The Figure which will be redraw
+    histogram_list : list
+        Contains objects the class:`histogram.Histogram`
+
+    label : The :class:`matplotlib.text.Text` instance.
     """
     __bins = None
     # zmienna używana przez wszystkie objekty tej klasy
     # inicjalizowana przez set_bins
 
     def __init__(self, fig, histogram_list, ax, label):
+        """
+        Parameters
+        ----------
+        fig : The :class:`matplotlib.figure.Figure`
+
+            The Figure which will be redraw.
+        ax : The :class:`matplotlib.axes.Axes`
+
+            The button renders into.
+        histogram_list : list
+
+            Contains objects the class:`histogram.Histogram`
+        label : The :class:`matplotlib.text.Text` instance.
+        """
         # Initialize parent constructor.
-        super(Bttn_bins, self).__init__(ax=ax, label=label)
+        super(ButtonBins, self).__init__(ax=ax, label=label)
         self.fig = fig  # For calling canvas draw which refreshes the image.
         self.histogram_list = histogram_list  # List with all histograms
         self.label = label
-        super(Bttn_bins, self).on_clicked(self.change_bins)  # Click reaction
+        super(ButtonBins, self).on_clicked(self.change_bins)  # Click reaction
 
+    @staticmethod
     def set_bins(bins):
+        """ Set the value of bins for all histograms
+
+        Parameters
+        ----------
+        bins : int
+
+            New value of bins
         """
-        Ustawia mi początkową wartość bins dla wszystkich
-        histogrmaów
-        """
-        Bttn_bins.__bins = bins
+        ButtonBins.__bins = bins
 
     def change_bins(self, event):
         """
-        Method for keyboard handling.
+        When the ButtonBins is clicked, call this func with event
+
+        Parameters
+        ----------
+        event : The class:`matplotlib.backend_bases.Event`
         """
         # Changing number of bins; Number is a power of 2. Max val. 512.
-
         if self.label == '+':
-            if Bttn_bins.__bins < 512:
-                Bttn_bins.__bins *= 2
+            if ButtonBins.__bins < 512:
+                ButtonBins.__bins *= 2
                 for his in self.histogram_list:
-                    his.set_bins(Bttn_bins.__bins)
+                    his.set_bins(ButtonBins.__bins)
                     his.update()
                     his.draw_green_space()
         elif self.label == '-':
-            if Bttn_bins.__bins > 2:
-                Bttn_bins.__bins /= 2
+            if ButtonBins.__bins > 2:
+                ButtonBins.__bins /= 2
                 for his in self.histogram_list:
-                    his.set_bins(Bttn_bins.__bins)
+                    his.set_bins(ButtonBins.__bins)
                     his.update()
                     his.draw_green_space()
         self.fig.canvas.draw()
 
 
 class Radio(RadioButtons):
-    """
-    class used in hdfsee
-    image - image created by functions imshow()
-    fig - object of the Figuge class used to redraw
-    cmap - color map used to change color map in image
+    """A GUI radio button used in hdfsee
+
+    Attributes
+    ----------
+    fig : The class:`matplotlib.figure.Figure`.
+
+        The Figure which will be redraw
+    image : The class:`matplotlib.image.AxesImage`
+
+            created by functions imshow()
+    cmap : The class:`matplotlib.colors.Colormap`
+
+        used to change color map in image
     """
     def __init__(self, fig, ax, labels, cmap, image):
+        """
+        Parameters
+        ----------
+        fig : The class:`matplotlib.figure.Figure`
+
+            The Figure which will be redraw.
+        ax : The class:`matplotlib.axes.Axes`
+
+            The button renders into.
+        labels : tuple of The :class:`matplotlib.text.Text`
+
+            instance.
+        cmap : The class:`matplotlib.colors.Colormap`
+
+            used to change color map in image
+        image : The class:`matplotlib.image.AxesImage`
+
+            created by functions imshow()
+        """
         self.image = image
         self.fig = fig
         self.cmap = cmap
@@ -70,8 +127,7 @@ class Radio(RadioButtons):
         self.on_clicked(self.colorfunc)
 
     def colorfunc(self, label):
-        """
-        Calls itself after switching colour.
+        """When the button is clicked, call this func with button label
         Updates the image.
         """
         # change name cmap
@@ -82,33 +138,71 @@ class Radio(RadioButtons):
         self.fig.canvas.draw()
 
     def set_image(self, image):
-        """
-        set a new object returned by imshow
+        """set a new image
+
+        Parameters
+        ----------
+        image : The class:`matplotlib.image.AxesImage`
+
+            created by functions imshow()
         """
         self.image = image
 
     def get_cmap(self):
-        """
-        returned last cmap
+        """returned last cmap
         """
         return self.cmap
 
 
-class My_slider(Slider):
-    """
-    class used in hdfsee
-    image - image created by functions imshow()
-    fig - object of the Figuge class used to redraw
-    vmax and vmin - is used to change contrast
+class ContrastSlider(Slider):
+    """A GUI slider used in hdfsee to change the contrast
+
+    Attributes
+    ----------
+    fig : The class:`matplotlib.figure.Figure`.
+
+        The Figure which will be redraw
+    image : The class:`matplotlib.image.AxesImage`
+
+            created by functions imshow()
+    vmax : int
+
+        define the data range that the colormap covers.
+    vmin : int
+
+        define the data range that the colormap covers.
     """
     def __init__(self, image, fig, ax, label, vmin, vmax):
+        """
+        Parameters
+        ----------
+        fig : The class:`matplotlib.figure.Figure`
+
+            The Figure which will be redraw.
+        ax : The class:`matplotlib.axes.Axes`
+
+            The button renders into.
+        label : The :class:`matplotlib.text.Text` instance.
+
+            instance.
+        vmax : int
+
+            define the data range that the colormap covers.
+        vmin : int
+
+            define the data range that the colormap covers.
+        image : The class:`matplotlib.image.AxesImage`
+
+            created by functions imshow()
+        """
         self.image = image
         self.fig = fig
         self.vmax = vmax
         self.vmin = vmin
         # Initialize parent constructor.
-        super(My_slider, self).__init__(ax, label, vmin, 600, valinit=255)
-        super(My_slider, self).on_changed(self.on_check)  # On click reaction.
+        super(ContrastSlider, self).__init__(ax, label, vmin, 600, valinit=255)
+        # On click reaction.
+        super(ContrastSlider, self).on_changed(self.on_check)
 
     def on_check(self, event):
         self.vmax = event
@@ -116,17 +210,54 @@ class My_slider(Slider):
         self.fig.canvas.draw()
 
     def set_image(self, image):
+        """set a new image
+
+        Parameters
+        ----------
+        image : The class:`matplotlib.image.AxesImage`
+
+            created by functions imshow()
+        """
         self.image = image
 
     def get_vmax(self):
+        """returned  last vmax
+
+        Returns
+        -------
+        vmax : int
+            max range that the colormap covers.
+        """
         return self.vmax
 
     def get_vmin(self):
+        """returned  last vmin
+
+        Returns
+        -------
+        vmin : int
+            min range that the colormap covers.
+        """
         return self.vmin
 
 
-class Bttn(Button):
-    """
+class CenteringButton(Button):
+    """A GUI button for changing
+    the color of the histogram bar for proper centering.
+
+    Attributes
+    ----------
+    fig : The class:`matplotlib.figure.Figure`.
+
+        The Figure which will be redraw
+    colors : itertools.cycle list
+
+        contains a list of colors
+    histogram_list : list
+
+        Contains objects the class:`histogram.Histogram`
+
+    define the data range that the colormap covers.
     Class for button changing crystal type.
     fig - whole image figure called by canvas.draw().
     list_color - list with def. colours.
@@ -137,19 +268,27 @@ class Bttn(Button):
     def __init__(self, fig, axs, label, list_color,
                  histogram_list, dict_color_histogram):
         # Initialize parent constructor.
-        super(Bttn, self).__init__(ax=axs, label=label, color=list_color[0])
+        super(CenteringButton, self).__init__(ax=axs, label=label,
+                                              color=list_color[0])
         self.fig = fig  # For calling canvas draw which refreshes the image.
         # Loop for the colour list of a given button.
         self.colors = itertools.cycle(list_color)
-        super(Bttn, self).on_clicked(self.on_check)  # Click reaction
+        # Click reaction
+        super(CenteringButton, self).on_clicked(self.on_check)
         self.histogram_list = histogram_list  # List with all histograms
         self.dict_color_histogram = dict_color_histogram
         self.list_color = list_color
 
     def on_check(self, label):
-        """
-        Change colour of the histogram depending
+        """Change colour of the histogram depending
         on which switch has been clicked.
+
+        Parameters
+        ----------
+
+        label : The :class:`matplotlib.text.Text` instance.
+
+            instance.
         """
         self.color = next(self.colors)  # After clicking changes the colour
         # to the next in a loop. For a given button the colour will be set in
@@ -164,6 +303,8 @@ class Bttn(Button):
         self.fig.canvas.draw()  # Redraw the current figure.
 
     def reset_color(self):
+        """Restore the initial settings
+        """
         self.colors = itertools.cycle(self.list_color)
         self.color = self.list_color[0]
         self.dict_color_histogram[self.label.get_text()] = self.color
@@ -175,12 +316,30 @@ class Bttn(Button):
 
 
 class Span():
+    """Visually select the region of interest on a single histogram
+
+    Attributes
+    ----------
+    fig : The class:`matplotlib.figure.Figure`.
+
+        The Figure which will be redraw
+    crystals_excluded_list : list
+
+        Crystals excluded
+    all_crystals_list : list
+
+        all crystals
+    histogram_list : list
+
+        Contains objects the class:`histogram.Histogram`
+    index : int
+
+        index number in the histgram_list
+    name : str
+
+        name of the histrgam
     """
-    Class for selecting the region of interest.
-    which_was_clicked_before - list of flags for showing
-        what was selected where.
-    dict_witch_range_crystal - original scope under as value (?).
-    """
+    # list of flags for showing what was selected last, where.
     __which_was_used_last = [False, False, False, False, False, False]
     __crystals_included_list = []
     # crystals in green space
@@ -201,22 +360,31 @@ class Span():
         self.span = SpanSelector(self.histogram_list[index].axs, self.onselect,
                                  'horizontal', useblit=True,
                                  rectprops=dict(alpha=0.5, facecolor='red'))
+
     @staticmethod
     def set_all_false():
+        """list of flags to show what was selected were set to false
+        """
         Span.__which_was_used_last = [False, False, False, False, False, False]
+
     @staticmethod
     def get_all_used():
+        """get a list of flags to show what was selected were.
+        """
         return Span.__which_was_used_last
 
     def onselect(self, xmin, xmax):
-        """
-        Method for getting data from a dictionary
-        after selecting region of interest.
+        """Selects the list of crystals whose parameter
+        is in the region of interest (xmin, xmax),
+        sets new data for histograms and draws the region of interest
+
+        One click reset clear the region of interest.
         """
         Span.set_all_false()
         Span.__which_was_used_last[self.index] = True
 
-        # Always search every crystal and exclude the rest (?).
+        # Always search every crystal whose parameter
+        # is in the region of interes and exclude the rest.
         # We clear list and search again
         Span.__crystals_included_list.clear()
         self.crystals_excluded_list.clear()
@@ -247,21 +415,37 @@ class Span():
         for hist in self.histogram_list:
             # Loop for changing the colour to green on the selected part.
             hist.draw_green_space()
+
     def is_exluded(self, crystal):
+        """checks whether a given crystal is in the region of interest.
+
+        Parameters
+        ----------
+
+        crystal : dict
+            crystal details.
+
+        Returns
+        -------
+
+        True : if crystal is in the region of interest.
+        """
         for hist in self.histogram_list:
             if hist.bool_crystal_exluded_green_space(crystal[hist.name]):
                 self.crystals_excluded_list.append(crystal)
                 return True
+
     @staticmethod
     def get_crystals_included_list():
+        """ Returns all crystals found in the region of interest
+        """
         return Span.__crystals_included_list
 
     def data_update(self):
-        """
-        Method for updating data in
+        """Method for updating data in
         the histograms with regard to the selection.
         """
-        dict_data_histogram_included =\
+        data_included =\
             crystlib.dict_data_histogram(Span.__crystals_included_list)
 
         data_excluded_a = []
@@ -279,21 +463,20 @@ class Span():
             data_excluded_alfa.append(crystal['alfa'])
             data_excluded_beta.append(crystal['beta'])
             data_excluded_gamma.append(crystal['gamma'])
-        self.histogram_list[0].set_data(data_to_histogram=dict_data_histogram_included["a"],
+        self.histogram_list[0].set_data(data_to_histogram=data_included["a"],
                                         data_excluded=data_excluded_a)
-        self.histogram_list[1].set_data(data_to_histogram=dict_data_histogram_included["b"],
+        self.histogram_list[1].set_data(data_to_histogram=data_included["b"],
                                         data_excluded=data_excluded_b)
-        self.histogram_list[2].set_data(data_to_histogram=dict_data_histogram_included["c"],
+        self.histogram_list[2].set_data(data_to_histogram=data_included["c"],
                                         data_excluded=data_excluded_c)
 
-        self.histogram_list[3].set_data(data_to_histogram=dict_data_histogram_included["alfa"],
+        self.histogram_list[3].set_data(data_to_histogram=data_included["alfa"],
                                         data_excluded=data_excluded_alfa)
 
-        self.histogram_list[4].set_data(data_to_histogram=dict_data_histogram_included["beta"],
+        self.histogram_list[4].set_data(data_to_histogram=data_included["beta"],
                                         data_excluded=data_excluded_beta)
 
-        self.histogram_list[5].set_data(data_to_histogram=dict_data_histogram_included["gamma"],
+        self.histogram_list[5].set_data(data_to_histogram=data_included["gamma"],
                                         data_excluded=data_excluded_gamma)
         for hist in self.histogram_list:
             hist.update()
-
