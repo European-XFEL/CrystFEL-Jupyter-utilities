cb46096f263b473b6239525f2f70b23dd1e591e3
Tomasz Sachanowski <tomaszsachanowski@gmail.com>
2019-07-08 09:46:06 +0200

    added a description

--- check-peak-detection.sh
+++ check-peak-detection.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-
+# script runs subsequent images via hdfsee.py from the stream file.
+# Running from args1: stream file, args2: geomfile
 for i in $(grep cxidb $1 | grep ^Image | cut -f 3 -d " ")
 do
   echo $i
