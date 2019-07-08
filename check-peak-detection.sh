3090dce5ed272e36f0b1dd3e95aa953b86609e18
Tomasz Sachanowski <tomaszsachanowski@gmail.com>
2019-07-08 09:48:19 +0200

    change parsing stream file

--- check-peak-detection.sh
+++ check-peak-detection.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # script runs subsequent images via hdfsee.py from the stream file.
 # Running from args1: stream file, args2: geomfile
-for i in $(grep cxidb $1 | grep ^Image | cut -f 3 -d " ")
+for i in $(grep "Image filename" $1| cut -f 3 -d " ")
 do
   echo $i
-  python hdfsee.py  ~/$i  -g $2 -p $1
+  python hdfsee.py  $i  -g $2 -p $1
 done
