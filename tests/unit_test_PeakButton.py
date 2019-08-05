bb74714cf549a74dc4f31ebf18b227268738b33f
Tomasz Sachanowski <tomaszsachanowski@gmail.com>
2019-08-05 14:26:59 +0200

    update test class PeakButton

--- tests/unit_test_PeakButton.py
+++ tests/unit_test_PeakButton.py
@@ -7,12 +7,11 @@ import unittest
 from unittest.mock import patch, Mock
 
 sys.path.insert(0, os.getcwd())
-from widget import PeakButton
+from widget import PeakButtons
 
 
-class Test_PeakButton(unittest.TestCase):
-    @patch('stream_read.PeakSearch')
-    @patch("stream_read.PeakReflections")
+class Test_PeakButtons(unittest.TestCase):
+    @patch('widget.Button')
     @patch('peak_h5.Peak')
     @patch('panel.Detector')
     @patch('widget.ContrastSlider')
@@ -20,113 +19,97 @@ class Test_PeakButton(unittest.TestCase):
     @patch('matplotlib.pyplot')
     @patch('matplotlib.axes.Axes')
     @patch('matplotlib.pyplot.axes')
-    def setUp(self, Mock_ax, Mock_axs, Mock_plt, Mock_radio, Mock_slider,
-              Mock_detector, Mock_peak, Mock_peak_reflections,
-              Mock_peak_search):
-
+    def setUp(self, Mock_axes, Mock_ax, Mock_plt, Mock_radio, Mock_slider,
+              Mock_detector, Mock_peak, Mock_button):
+        self.Mock_axes = Mock_axes
         self.Mock_ax = Mock_ax
-        self.Mock_axs = Mock_axs
         self.Mock_plt = Mock_plt
         self.Mock_radio = Mock_radio
         self.Mock_slider = Mock_slider
         self.Mock_detector = Mock_detector
         self.Mock_peak = Mock_peak
-        self.Mock_peak_reflections = Mock_peak_reflections
-        self.Mock_peak_search = Mock_peak_search
+        self.Mock_button = Mock_button
 
-        self.mock_peak_search = self.Mock_peak_search()
-        self.mock_peak_reflections = self.Mock_peak_reflections()
-        self.mock_plt = self.Mock_plt()
-        self.mock_fig = self.mock_plt.figure()
-        self.mock_axs = self.Mock_axs()
-        self.mock_ax = self.Mock_ax()
-        self.mock_radio = self.Mock_radio()
-        self.mock_slider = self.Mock_slider()
-        self.mock_detector = self.Mock_detector()
-        self.mock_peak = self.Mock_peak()
-        self.list_active_mock_peak = [True, True, False]
+        self.mock_fig = self.Mock_plt.figure
         self.title = "test.title"
-        self.label = "test.label"
-        self.mock_detectors = {"det1": self.mock_detector, "det2":
-                               self.mock_detector}
-        self.mock_peaks = [self.mock_peak, self.mock_peak,
-                           self.mock_peak]
-        self.axis_list = [self.Mock_ax(), self.Mock_ax(), self.Mock_ax()]
+        self.mock_detectors = {"det1": self.Mock_detector, "det2":
+                               self.Mock_detector}
+        self.mock_peaks = [self.Mock_peak, self.Mock_peak,
+                           self.Mock_peak]
         self.matrix = numpy.ones((2, 3))
-        self.bttn = PeakButton(
-            fig=self.mock_fig, axs=self.mock_axs, matrix=self.matrix,
-            ax=self.mock_ax, label=self.label, axis_list=self.axis_list,
-            list_active_peak=self.list_active_mock_peak,
-            peaks=self.mock_peaks, detectors=self.mock_detectors,
-            title=self.title, radio=self.mock_radio, slider=self.mock_slider)
+        self.bttn = PeakButtons(
+            fig=self.mock_fig, ax=self.Mock_ax, matrix=self.matrix,
+            peaks=self.mock_peaks, panels=self.mock_detectors,
+            number_peaks_button=3, title=self.title,
+            radio=self.Mock_radio, slider=self.Mock_slider)
 
     def test_init(self):
-        assert self.mock_plt.figure.called
-        assert self.Mock_plt.called
-        assert self.Mock_ax.called
-        assert self.Mock_axs.called
-        assert self.Mock_radio
-        assert self.Mock_slider
-        assert self.Mock_detector
-        assert self.Mock_peak
-        self.assertEqual(self.Mock_ax.call_count, 4)
-        self.assertEqual(self.Mock_radio.call_count, 1)
-        self.assertEqual(self.Mock_peak.call_count, 1)
+        self.assertEqual(self.Mock_axes.call_count, 3)
+        self.assertEqual(self.Mock_button.call_count, 3)
+        self.Mock_button.assert_called_with(
+            ax=self.Mock_axes(), label='peaks_reflections on/off')
+        self.assertListEqual(
+            self.bttn.buttons, [self.Mock_button(), self.Mock_button(),
+                                self.Mock_button()])
 
-    def visual_peaks_reflection(self):
-        self.mock_peak_reflections.get_position.return_value = (1, 2)
-        self.mock_detector.get_peaks_reflection.return_value = \
-            [self.mock_peak_reflections, self.mock_peak_reflections,
-             self.mock_peak_reflections]
+    @patch('widget.plt.Circle')
+    def test_visual_peaks_reflection(self, mock_circle):
+        self.Mock_detector.get_peaks_reflection.return_value = \
+            [{'position': (1, 2)}, {'position': (3, 1)}, {'position': (2, 2)}]
 
         self.bttn.visual_peaks_reflection()
 
-        assert self.mock_detector.get_peaks_reflection.called
-        assert self.mock_peak_reflections.get_position.called
-        assert self.mock_axs.add_artist.called
-        self.assertEqual(self.mock_axs.add_artist.call_count, 6)
+        self.assertEqual(self.Mock_detector.get_peaks_reflection.call_count, 2)
+        mock_circle.assert_called_with((2, 2), color='r', fill=False, radius=5)
+        self.Mock_ax.add_artist.assert_called_with(mock_circle())
+        self.assertEqual(self.Mock_ax.add_artist.call_count, 6)
+        self.assertEqual(self.bttn.list_active_peak, [False, False, True])
 
-    def test_visual_peaks_search(self):
-        self.mock_peak_search.get_position.return_value = (1, 3)
-        self.mock_detector.get_peaks_search.return_value = \
-            [self.mock_peak_search, self.mock_peak_search,
-             self.mock_peak_search, self.mock_peak_search]
+    @patch('widget.plt.Circle')
+    def test_visual_peaks_search(self, mock_circle):
+        self.Mock_detector.get_peaks_search.return_value = \
+            [{'position': (3, 1)}, {'position': (8, 9)}]
 
         self.bttn.visual_peaks_search()
-        assert self.mock_detector.get_peaks_search.called
-        assert self.mock_peak_search.get_position.called
-        assert self.mock_axs.add_artist.called
-        self.assertEqual(self.mock_axs.add_artist.call_count, 8)
+        self.assertEqual(self.Mock_detector.get_peaks_search.call_count, 2)
+        mock_circle.assert_called_with((8, 9), color='g', fill=False, radius=5)
+        self.Mock_ax.add_artist.assert_called_with(mock_circle())
+        self.assertEqual(self.Mock_ax.add_artist.call_count, 4)
+        self.assertEqual(self.bttn.list_active_peak, [False, True, False])
 
-    def test_visual_peaks(self):
-        self.mock_peak.get_position.return_value = (1, 2)
+    @patch('widget.plt.Circle')
+    def test_visual_peaks(self, mock_circle):
+        self.Mock_peak.get_position.return_value = (1, 2)
 
         self.bttn.visual_peaks()
-        assert self.mock_peak.get_position.called
-        assert self.mock_axs.add_artist.called
-        self.assertEqual(self.mock_axs.add_artist.call_count, 3)
+
+        self.assertEqual(self.Mock_peak.get_position.call_count, 3)
+        self.assertEqual(self.Mock_ax.add_artist.call_count, 3)
+        mock_circle.assert_called_with((1, 2), color='y', fill=False, radius=5)
+        self.Mock_ax.add_artist.assert_called_with(mock_circle())
+        self.assertEqual(self.Mock_ax.add_artist.call_count, 3)
+        self.assertEqual(self.bttn.list_active_peak, [False, False, False])
 
     @patch('matplotlib.backend_bases.Event')
     def test_peaks_on_of(self, Mock_event):
-        event = Mock_event()
-        self.mock_radio.get_cmap.return_value = 'inferno'
-        self.mock_slider.get_vmax.return_value = 400
-        self.mock_slider.get_vmin.return_value = 100
-        self.bttn.peaks_on_of(event)
+        self.Mock_radio.get_cmap.return_value = 'inferno'
+        self.Mock_slider.get_vmax.return_value = 400
+        self.Mock_slider.get_vmin.return_value = 100
+        self.bttn.peaks_on_of(Mock_event)
 
-        assert self.mock_axs.cla.called
-        assert self.mock_radio.get_cmap.called
-        assert self.mock_slider.get_vmax.called
-        assert self.mock_slider.get_vmin.called
-        assert self.mock_axs.imshow.called
-        self.mock_axs.imshow.assert_called_with(self.matrix, animated=True,
-                                                cmap='inferno', vmax=400,
-                                                vmin=100)
+        assert self.Mock_ax.cla.called
+        assert self.Mock_radio.get_cmap.called
+        assert self.Mock_slider.get_vmax.called
+        assert self.Mock_slider.get_vmin.called
+        assert self.Mock_ax.imshow.called
+        self.Mock_ax.imshow.assert_called_with(self.matrix, animated=True,
+                                               cmap='inferno', vmax=400,
+                                               vmin=100)
         assert self.mock_fig.canvas.draw.called
-        assert self.mock_axs.set_title.called
-        self.mock_axs.set_title.assert_called_with(self.title)
-        assert self.mock_radio.set_image.called
-        assert self.mock_slider.set_image.called
+        assert self.Mock_ax.set_title.called
+        self.Mock_ax.set_title.assert_called_with(self.title)
+        assert self.Mock_radio.set_image.called
+        assert self.Mock_slider.set_image.called
 
 if __name__ == '__main__':
         unittest.main()
